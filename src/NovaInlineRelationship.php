d11adb37bada094c22cdb2a9828e707086166dce
Navneet Rai <navneet@kirschbaumdevelopment.com>
2019-07-24 14:59:18 -0400

    Updated NovaInline Relationship Component

--- src/NovaInlineRelationship.php
+++ src/NovaInlineRelationship.php
@@ -2,7 +2,9 @@
 
 namespace KirschbaumDevelopment\NovaInlineRelationship;
 
+use Illuminate\Support\Str;
 use Laravel\Nova\Fields\Field;
+use Illuminate\Http\UploadedFile;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use KirschbaumDevelopment\NovaInlineRelationship\Rules\RelationshipRule;
 
@@ -25,8 +27,20 @@ class NovaInlineRelationship extends Field
      */
     public function resolve($resource, $attribute = null)
     {
+        parent::resolve($resource, $attribute);
+
         $propMap = $resource::getPropertyMap();
         $properties = $propMap[$attribute ?? $this->attribute];
+
+        $properties = collect($properties)->map(function ($value, $key) {
+            return $this->setMetaFromClass($value, $key);
+        })->all();
+
+        $this->value = collect($this->value)->map(function ($items, $id) use ($properties) {
+            return collect($items)->map(function ($value, $key) use ($properties) {
+                return $this->setMetaFromClass($properties[$key] ?? [], $key, $value);
+            })->all();
+        })->all();
 
         $this->rules = [$this->getRelationshipRule($attribute ?? $this->attribute, $properties)];
 
@@ -40,8 +54,33 @@ class NovaInlineRelationship extends Field
             'settings' => $properties,
             'singular' => $resource->isSingularRelationship($attribute ?? $this->attribute),
         ]);
+    }
 
-        parent::resolve($resource, $attribute);
+    protected function setMetaFromClass(array $item, $attrib, $value = null)
+    {
+        $attrs = ['name' => $attrib, 'attribute' => $attrib];
+
+        /** @var Field $class */
+        $class = app($item['component'], $attrs);
+        $class->value = $value !== null ? $value : '';
+
+        if (! empty($item['options']) && is_array($item['options'])) {
+            $class->withMeta($item['options']);
+        }
+
+        if (! empty($item['placeholder'])) {
+            $class->withMeta(['extraAttributes' => [
+                'placeholder' => $item['placeholder'],
+            ]]);
+        }
+
+        $item['meta'] = $class->jsonSerialize();
+        // We are using Singular Label instead of name to display labels as compound name will be used in Vue
+        $item['meta']['singularLabel'] = Str::studly($item['label'] ?? $attrib);
+
+        $item['meta']['placeholder'] = 'Add ' . Str::studly($item['label'] ?? $attrib);
+
+        return $item;
     }
 
     /**
@@ -57,9 +96,49 @@ class NovaInlineRelationship extends Field
     protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
     {
         if ($request->exists($requestAttribute)) {
-            $value = json_decode($request[$requestAttribute], true);
-            $model->{$attribute} = $this->isNullValue($value) ? null : $value;
+            $response = is_array($request[$requestAttribute]) ? $request[$requestAttribute] : json_decode($request[$requestAttribute], true);
+
+            $propMap = $model::getPropertyMap();
+            $properties = $propMap[$attribute ?? $this->attribute];
+
+            $modResponse = collect($response)->map(function ($item) use ($properties, $request) {
+                return collect($item)->map(function ($value, $key) use ($properties, $item, $request) {
+                    if (! empty($properties[$key])) {
+                        $class = $this->getFieldClassFromProps($properties[$key], $key);
+
+                        $temp = new \stdClass();
+
+                        $files = collect($item)->filter(function ($itemData) {
+                            return $itemData instanceof UploadedFile;
+                        })->all();
+
+                        $newRequest = NovaInlineRelationshipRequest::createFrom($request)->duplicate($item);
+                        $newRequest->updateFiles($files);
+
+                        $class->fillAttribute($newRequest, $key, $temp, $key);
+
+                        return $temp->{$key} ?? null;
+                    }
+
+                    return $value;
+                })->all();
+            })->all();
+
+            $model->{$attribute} = $this->isNullValue($modResponse) ? null : $modResponse;
         }
+    }
+
+    /**
+     * @param array $props
+     * @param string $key
+     *
+     * @return Field
+     */
+    protected function getFieldClassFromProps($props, $key)
+    {
+        $attrs = ['name' => $key, 'attribute' => $key];
+
+        return app($props['component'], $attrs);
     }
 
     /**
