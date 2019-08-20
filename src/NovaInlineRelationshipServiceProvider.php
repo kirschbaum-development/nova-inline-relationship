adaf55f38c11a6f1af471e83537933e8481d5fa3
Navneet Rai <navneet@kirschbaumdevelopment.com>
2019-08-20 08:27:38 -0400

    Passed resource class to NovaInlineRelationship

--- src/NovaInlineRelationshipServiceProvider.php
+++ src/NovaInlineRelationshipServiceProvider.php
@@ -6,7 +6,6 @@ use Laravel\Nova\Nova;
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Nova\Fields\ResourceRelationshipGuesser;
 
 class NovaInlineRelationshipServiceProvider extends ServiceProvider
 {
@@ -23,11 +22,7 @@ class NovaInlineRelationshipServiceProvider extends ServiceProvider
         });
 
         Field::macro('inline', function () {
-            //$parent = debug_backtrace()[0]['class'];
-            //dd(ResourceRelationshipGuesser::guessResource('profile'));
-            $inlineClass = sprintf('%s', get_class($this));
-
-            return NovaInlineRelationship::make($this->name, $this->attribute);
+            return NovaInlineRelationship::make($this->name, $this->attribute)->resourceClass($this->resourceClass);
         });
     }
 
