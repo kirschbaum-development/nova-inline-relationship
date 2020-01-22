2dcc54fa64d3b4fe8ed0f8b1b9595413d9fcd5cf
wize-wiz <9093559+wize-wiz@users.noreply.github.com>
2020-01-22 22:24:39 +0100

    Removing comments, it clearly says BelongsToObserver :D

--- src/Observers/BelongsToObserver.php
+++ src/Observers/BelongsToObserver.php
@@ -8,30 +8,27 @@ use Illuminate\Support\Facades\Log;
 class BelongsToObserver extends BaseObserver
 {
     /**
-     * {@inheritdoc} Doc did not inherit ... :)
+     * {@inheritdoc}
      */
     public function updating(Model $model, $attribute, $value)
     {
-        // @note: why first() ?
         $parentModel = $model->{$attribute}()->first();
 
         if (empty($parentModel)) {
             return $this->creating($model, $attribute, $value);
         }
 
-        // @note: what about multiple $models updating? MorphToMany etc?
         if (count($value)) {
             $parentModel->update($value[0]);
         }
     }
 
     /**
-     * {@inheritdoc} Doc did not inherit ... :)
+     * {@inheritdoc}
      */
     public function creating(Model $model, $attribute, $value)
     {
         if (count($value)) {
-            // @note same as updating.
             $parentModel = $model->{$attribute}()->getRelated()->newInstance($value[0]);
             $parentModel->save();
             $model->{$attribute}()->associate($parentModel);
