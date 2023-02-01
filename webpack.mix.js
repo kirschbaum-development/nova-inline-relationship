8429bf755c72875a41822ac7a7e2f008d31ae80f
Wilker Mesquita <wilker@kirschbaumdevelopment.com>
2023-02-01 16:42:02 -0300

    Enabling Nova4 and updating package.json and composer.json files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,15 @@
-let mix = require('laravel-mix')
+let mix = require('laravel-mix');
+let path = require('path');
+
+mix.alias({
+    'laravel-nova$': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
+    '@': path.join(__dirname, 'vendor/laravel/nova/resources/js')
+});
+
+require('./mix');
 
 mix.setPublicPath('dist')
     .js('resources/js/field.js', 'js')
-    .sass('resources/sass/field.scss', 'css')
+    .vue({ version: 3 })
+    .css('resources/css/field.css', 'css')
+    .nova('kirschbaum-development/inline-select');
